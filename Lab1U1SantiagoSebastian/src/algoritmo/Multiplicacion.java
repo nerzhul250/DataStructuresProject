6837828ef3f8f1d010c40273750c76cd9fa3449b
nerzhul250 <gamersav1822@gmail.com>
2018-01-27 15:57:56 -0500

    stuff2

--- Lab1U1SantiagoSebastian/src/algoritmo/Multiplicacion.java
+++ Lab1U1SantiagoSebastian/src/algoritmo/Multiplicacion.java
@@ -7,4 +7,5 @@ public class Multiplicacion {
 
 }
 //No borrar
-		//
+//tabla de sustracion, j-i mod 10 es el indice donde se encuentra el resultado de
+// una sustracion elemental en el arreglo {0 9 8 7 6 5 4 3 2 1}
