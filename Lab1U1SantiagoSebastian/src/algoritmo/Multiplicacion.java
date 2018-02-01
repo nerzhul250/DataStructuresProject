90491c9689d28065c690b27269fe146dcab5b806
nerzhul250 <gamersav1822@gmail.com>
2018-02-01 14:06:38 -0500

    better

--- Lab1U1SantiagoSebastian/src/algoritmo/Multiplicacion.java
+++ Lab1U1SantiagoSebastian/src/algoritmo/Multiplicacion.java
@@ -36,19 +36,27 @@ public class Multiplicacion {
 		  if(A.length()==1 || B.length()==1){
 			  if(A.length()==1 && A.charAt(0)=='0' ||
 					  B.length()==1 && B.charAt(0)=='0'){
+				  
+		          //////////////
 				  bloque.append("respuesta: 0");
 				  bloque.append("\n");
 				  procedimiento.add(bloque.toString());
+		          //////////////
+				  
 				  return "0";
 			  }
 			  if(A.length()==1 && B.length()==1){
 				  int a=Integer.parseInt(A);
 				  int b=Integer.parseInt(B);
 				  String respuesta= (a*b)+"";
+				  
+		          //////////////
 				  bloque.append("respuesta: ");
 				  bloque.append(respuesta);
 				  bloque.append("\n");
 				  procedimiento.add(bloque.toString());
+		          //////////////
+				  
 				  return respuesta;
 			  }
 		  }
