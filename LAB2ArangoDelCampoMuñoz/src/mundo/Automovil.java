b4ff2aaa67e2756ff211698abb86d7d3e38b3da9
nerzhul250 <gamersav1822@gmail.com>
2018-03-02 17:14:54 -0500

    f

--- LAB2ArangoDelCampoMuñoz/src/mundo/Automovil.java
+++ LAB2ArangoDelCampoMuñoz/src/mundo/Automovil.java
@@ -9,7 +9,6 @@ public class Automovil {
 	public int hashCode() {
 		char[] caracteres=placa.toCharArray();
 		int modulus=(int) Math.pow(2,30);
-		int k=2;
 		long hash=0;
 		for (int i = 0; i <caracteres.length; i++) {
 			hash+=((Math.pow(2,i%30)%modulus)*(caracteres[i]%modulus))%modulus;
